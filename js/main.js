a96f65bdce8e769aa5f5bd320503b1637b4c70b0
Zacksob <alvaroresifonterealeaga@gmail.com>
2024-01-03 18:19:47 -0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function () {
+    $('#telefone').mask('(00) 00000-0000');
+
+    $('form').validate({
+        rules: {
+            nome: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            telefone: {
+                required: true
+            },
+            mensagem: {
+                required: true
+            },
+        },
+        mensagem: {
+            nome: 'Por favor, insira o seu nome'
+        },
+        submitHandler: function (form) {
+            console.log(form);
+        },
+        invalidHandler: function (evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+});
